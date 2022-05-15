d075b8d65a5870211ae11d83aa710f93b5d0f82e
Nilesh Patharkar <nilu.patharkar@gmail.com>
2022-05-15 20:53:25 +0530

    Testing for Conflict

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("I am back into the world !!!");
+		int i = 5 + 6;
+		int j = i;
+		System.out.println("Line1");
+		System.out.println("Line2");
+		int k;
+		System.out.println("Line3");
+		k = i + j;
+		System.out.println("Line4");
+		System.out.println(k);
+		k = 10;
+		System.out.println("Line5");
+		System.out.println(k);
+		
+	}
+
+}
